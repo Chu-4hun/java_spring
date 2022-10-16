5e86a1fca56e31968f97a66832df0aac69b0bccf
Chu-4hun <Tapok134224@mail.ru>
2022-10-16 20:31:10 +0300

    MovieController fix tnx @StounhandJ

--- src/main/java/com/hludencov/java_spring/controllers/MovieController.java
+++ src/main/java/com/hludencov/java_spring/controllers/MovieController.java
@@ -3,13 +3,14 @@ package com.hludencov.java_spring.controllers;
 import com.hludencov.java_spring.models.Movie;
 import com.hludencov.java_spring.repo.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-
+@Controller
 public class MovieController {
     @Autowired
     MovieRepository movieRepository;
